d9200eaf9a967ae2c7b392993b6656725ee59869
Sahil Sangwan <sah.sangwan@gmail.com>
2022-04-30 14:36:04 -0400

    reformatted route controller import in server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import routeController from "./controllers/routeController.js";
+const routeController = require("./controllers/routeController.js");
 import mongoose from "mongoose";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
     || 'mongodb://localhost:27017/webdev'
