3917944b3199fa646393ab4e5b2c6ff9cd994ed4
Sahil Sangwan <sah.sangwan@gmail.com>
2022-04-30 16:04:16 -0400

    new user schema fields

--- userSchema.js
+++ userSchema.js
@@ -3,6 +3,9 @@ const userSchema = mongoose.Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     firstName: String,
-    lastName: String
+    lastName: String,
+    cyclist: Boolean,
+    age: Number,
+    city: String
 }, {collection: "users"})
 module.exports = userSchema;
