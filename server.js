b9f322fa2d95aaf845b9158883c120bf820d80e2
Sahil Sangwan <sah.sangwan@gmail.com>
2022-05-01 23:29:50 -0400

    restore http

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: 'https://localhost:3000'
+    origin: 'http://localhost:3000'
 }));
 app.use(express.json());
 
